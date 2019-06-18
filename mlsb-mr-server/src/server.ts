1370f1875d769ccbd93dcf8fbb9ef2f4c199e531
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-06-19 00:03:27 +0200

    fix path in websocket server

--- mlsb-mr-server/src/server.ts
+++ mlsb-mr-server/src/server.ts
@@ -5,10 +5,14 @@ import * as WebSocket from "ws";
 import { config } from "./config";
 //
 
-const wss = new WebSocket.Server({ port: config.port });
+const wss = new WebSocket.Server({ port: config.port, path: "/" });
 
 wss.on("connection", function connection(ws) {
     ws.on("message", function incoming(message) {
         console.log("<WS>%s ", message);
     });
+});
+
+wss.on("error", function connection(error) {
+    console.error(error);
 });
