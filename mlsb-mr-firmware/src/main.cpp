bb1c8ff9b72d124a6b6d2326831cce8048d1a33a
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-06-19 00:03:07 +0200

    Working websocket firmware

--- mlsb-mr-firmware/src/main.cpp
+++ mlsb-mr-firmware/src/main.cpp
@@ -1,9 +1,40 @@
 #include <Arduino.h>
+#include "WebSocketClient.h"
+#include "ESP8266WiFi.h"
+
+WebSocketClient ws(false);
+
+const char wifiSSID[] = "UPCF18384A";
+const char wifiPassword[] = "";
+const char websocketHost[] = "192.168.0.11";
+const int websocketPort = 4114;
+
 
 void setup() {
-  // put your setup code here, to run once:
+	Serial.begin(115200);
+	WiFi.begin(wifiSSID, wifiPassword);
+
+	Serial.print("Connecting");
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
 }
 
+
+uint8_t i = 0;
+
+char msgbuf[40];
+
 void loop() {
-  // put your main code here, to run repeatedly:
+	if (!ws.isConnected()) {
+		Serial.println("ws_conn_pend");
+		ws.connect(websocketHost, "/", websocketPort);
+	} else {
+		Serial.println("ws_send");
+    	sprintf(msgbuf, "Hello %d", i++);
+		ws.send(msgbuf);
+		Serial.println("ws_send_done");
+	}
+	delay(500);
 }
